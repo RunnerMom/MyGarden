e68209c3112420d838512919cfbc40dbe93fc603
Egoyibo <jessymong@gmail.com>
2013-10-26 03:35:22 -0700

    updated gecoder

--- geocoder.py
+++ geocoder.py
@@ -1,37 +1,21 @@
-from pygeocoder import Geocoder
 import requests
 
-buyer = "400 Magnolia Street, Orangeburg, SC, 29115"
-buyer_add = buyer.replace(" ","+")
+#This list will store all the distances calculated so that it can be passed onto Jinja
+distances = []
 
+def get_distance_from_addresses(buyer_address, seller_address):
+	buyer_add = buyer_address.replace(" ","+")
+	seller_add = seller_address.replace(" ","+")
 
-sellers = ["110 Castro St, Mountain View, CA", "2 Moneta Ct, San Francisco, CA"]
-for seller in sellers:
-	seller_add = seller.replace(" ","+")
 	url = "http://maps.googleapis.com/maps/api/distancematrix/json?origins="+buyer_add+"&destinations="+seller_add+"&units=imperial&language=en&sensor=false"
-
 	results = requests.get(url)
 	results_json = results.json()
 
-	distance = (results_json["rows"][0]["elements"][0]["distance"]["text"]).replace(",",".")
-	print distance
+	if results_json["rows"][0]["elements"][0]["status"] != "OK":
+		distance = "No possible driving route!"
+	else:
+		distance = (results_json["rows"][0]["elements"][0]["distance"]["text"])
+	distances.append(distance)
 
-
-
-
-
-#Why doesn't the code work with lat and long?
-
-# buyer_gcode = Geocoder.geocode(buyer)
-# buyer_lat = buyer_gcode[0].coordinates[0]
-# buyer_lng = buyer_gcode[0].coordinates[1]
-
-	# results = Geocoder.geocode(seller)
-	# lat = results[0].coordinates[0]
-	# lng = results[0].coordinates[1]
-
-	# print lat
-	# print lng
-	# url = "http://maps.googleapis.com/maps/api/distancematrix/json?origins=%r,%r&destinations=%r,%r&language=fr-FR&sensor=false" % (buyer_lat, buyer_lat, lat, lng)
-
-	
+	# return distances
+	#Now the distances list can be passed onto Jinja and where it can be iterated over
