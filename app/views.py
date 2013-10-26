285701b9b72259716dd4a18cd690104d893a7912
Aimee Morgan <alm@poshlsot.(none)>
2013-10-26 11:24:34 -0700

    login

--- app/views.py
+++ app/views.py
@@ -68,7 +68,6 @@ def login():
         return render_template('login.html')
 
 @app.route('/logout')
-@login_required
 def logout():
         logout_user()
         return redirect('/')
@@ -82,61 +81,59 @@ client = oauth.Client(consumer)
 
 #If the user needs to give authorization, request a request token and create a link to Linkedin.
 
-@app.route('/oauth', methods=['POST'])
-def request_oauth():
-    request_token_url = 'https://api.linkedin.com/uas/oauth/requestToken'
-    resp, content = client.request(request_token_url, "POST")
-    if resp['status'] != '200':
-        raise Exception("Invalid response %s." % resp['status'])
+# @app.route('/oauth', methods=['POST'])
+# def request_oauth():
+#     request_token_url = 'https://api.linkedin.com/uas/oauth/requestToken'
+#     resp, content = client.request(request_token_url, "POST")
+#     if resp['status'] != '200':
+#         raise Exception("Invalid response %s." % resp['status'])
 
-@app.route('/request_oauth')
-def request_oauth():
+# @app.route('/request_oauth')
+# def request_oauth():
 
-    request_token_url = 'https://api.linkedin.com/uas/oauth/requestToken'
-    resp, content = client.request(request_token_url, "POST")
-    if resp['status'] != '200':
-        raise Exception("Invalid response %s." % resp['status'])
+#     request_token_url = 'https://api.linkedin.com/uas/oauth/requestToken'
+#     resp, content = client.request(request_token_url, "POST")
+#     if resp['status'] != '200':
+#         raise Exception("Invalid response %s." % resp['status'])
 
      
-    request_token = dict(urlparse.parse_qsl(content))
+#     request_token = dict(urlparse.parse_qsl(content))
 
-    session['request_token'] = request_token['oauth_token']
-    session['request_token_secret'] = request_token['oauth_token_secret']
-    #Create a Linkedin link containing the request token.
-    authorize_link = '%s?oauth_token=%s' % (AUTHORIZE_URL,request_token['oauth_token'])
-    return render_template("request_oauth.html", auth=authorize_link)
+#     session['request_token'] = request_token['oauth_token']
+#     session['request_token_secret'] = request_token['oauth_token_secret']
+#     #Create a Linkedin link containing the request token.
+#     authorize_link = '%s?oauth_token=%s' % (AUTHORIZE_URL,request_token['oauth_token'])
+#     return render_template("request_oauth.html", auth=authorize_link)
 
-#After the user has authorized, Linkedin will generate another token and redirect back to this route.
-@app.route('/access')
-def get_access():
-    access_token_url = 'https://api.linkedin.com/uas/oauth/accessToken'
+# #After the user has authorized, Linkedin will generate another token and redirect back to this route.
+# @app.route('/access')
+# def get_access():
+#     access_token_url = 'https://api.linkedin.com/uas/oauth/accessToken'
 
-    token = oauth.Token(session['request_token'], session['request_token_secret'])
-    token.set_verifier(oauth_verifier)
-    client = oauth.Client(consumer, token)
+#     token = oauth.Token(session['request_token'], session['request_token_secret'])
+#     token.set_verifier(oauth_verifier)
+#     client = oauth.Client(consumer, token)
      
-    resp, content = client.request(access_token_url, "POST")
-    access_token = dict(urlparse.parse_qsl(content))
+#     resp, content = client.request(access_token_url, "POST")
+#     access_token = dict(urlparse.parse_qsl(content))
 
 
-
-    session['access_token'] = access_token['oauth_token']
-    session['access_token_secret'] = access_token['oauth_token_secret']
-    return redirect('/profile')
 
 #     session['access_token'] = access_token['oauth_token']
 #     session['access_token_secret'] = access_token['oauth_token_secret']
-#     return redirect('/')
+#     return redirect('/profile')
+
+# #     session['access_token'] = access_token['oauth_token']
+# #     session['access_token_secret'] = access_token['oauth_token_secret']
+# #     return redirect('/')
 
 
-@app.route('/profile')
-@login_required
+@app.route('/profile', methods=['POST'])
 def profile():
     return render_template('base.html', user=user)
 
 
 @app.route('/buyProduct')
-@login_required
 def buy_product():
     return render_template('buy.html', user=user)
 
